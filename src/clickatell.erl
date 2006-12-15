af956f0c988f7bc6a2e53dc0a27e016d04b5ffcd
Hoan Ton-That <hoan.tonthat@gmail.com>
2006-12-15 08:37:31 +0000

    Make HTTP requests asynchronous

--- src/clickatell.erl
+++ src/clickatell.erl
@@ -7,7 +7,6 @@
 -export([balance/0,
          check/1,
          cost/1,
-         ping/0,
          send/2,
          status/1]).
 -export([init/1,
@@ -19,6 +18,8 @@
          ping_loop/2]).
 -export([arg_to_sms/1,
          handle/2]).
+
+-record(state, {session_id, callback_ets}).
 
 -define(BASE_URL,  "https://api.clickatell.com").
 -define(PING_WAIT, timer:minutes(5)).
@@ -40,9 +41,6 @@ check(To) ->
 cost(MessageID) ->
   gen_server:call(?MODULE, {cost, MessageID}).
 
-ping() ->
-  gen_server:call(?MODULE, {ping}).
-
 send(To, Message) ->
   gen_server:call(?MODULE, {send, To, Message}).
 
@@ -52,30 +50,51 @@ status(MessageID) ->
 %% Server
 init([User, Pass, API]) ->
   process_flag(trap_exit, true),
-  case call_login(User, Pass, API) of
-    {error, Error} ->
-      {stop, Error};
-    SessionID ->
+  case login(User, Pass, API) of
+    {ok, SessionID} ->
       spawn_link(?MODULE, ping_loop, [?PING_WAIT, SessionID]),
-      {ok, SessionID}
+      {ok, #state{session_id = SessionID, callback_ets = ets:new(callbacks, [])}};
+    {error, Error} ->
+      {stop, Error}
   end.
 
-handle_call({balance}, _From, SessionID) ->
-  {reply, call_balance(SessionID), SessionID};
-handle_call({check, To}, _From, SessionID) ->
-  {reply, call_check(To, SessionID), SessionID};
-handle_call({cost, MessageID}, _From, SessionID) ->
-  {reply, call_cost(MessageID, SessionID), SessionID};
-handle_call({ping}, _From, SessionID) ->
-  {reply, call_ping(SessionID), SessionID};
-handle_call({send, To, Message}, _From, SessionID) ->
-  {reply, call_send(To, Message, SessionID), _From, SessionID};
-handle_call({status, MessageID}, _From, SessionID) ->
-  {reply, call_status(MessageID, SessionID), _From, SessionID}.
+handle_call({balance}, From, State) ->
+  {ok, RequestID} = call_balance(State#state.session_id),
+  ets:insert(State#state.callback_ets, {RequestID, {balance, From}}),
+  {noreply, State};
+handle_call({check, To}, From, State) ->
+  {ok, RequestID} = call_check(To, State#state.session_id),
+  ets:insert(State#state.callback_ets, {RequestID, {check, From}}),
+  {noreply, State};
+handle_call({cost, MessageID}, From, State) ->
+  {ok, RequestID} = call_cost(MessageID, State#state.session_id),
+  ets:insert(State#state.callback_ets, {RequestID, {cost, From}}),
+  {noreply, State};
+handle_call({send, To, Message}, From, State) ->
+  {ok, RequestID} = call_send(To, Message, State#state.session_id),
+  ets:insert(State#state.callback_ets, {RequestID, {send, From}}),
+  {noreply, State};
+handle_call({status, MessageID}, From, State) ->
+  {ok, RequestID} = call_status(MessageID, State#state.session_id),
+  ets:insert(State#state.callback_ets, {RequestID, {status, From}}),
+  {noreply, State}.
 
 handle_cast(stop, State) ->
   {stop, normal, State}.
 
+handle_info({http, {RequestID, HTTPResponse}}, State) ->
+  {{_,200,_},_,ResponseText} = HTTPResponse,
+  Response               = parse_response(binary_to_list(ResponseText)),
+  [{_, {Command, From}}] = ets:lookup(State#state.callback_ets, RequestID),
+  case Command of
+    balance -> gen_server:reply(From, handle_balance(Response));
+    check   -> gen_server:reply(From, handle_check(Response));
+    cost    -> gen_server:reply(From, handle_cost(Response));
+    send    -> gen_server:reply(From, handle_send(Response));
+    status  -> gen_server:reply(From, handle_status(Response))
+  end,
+  ets:delete(State#state.callback_ets, RequestID),
+  {noreply, State};
 handle_info({'EXIT', {ping_failed, Error}}, State) ->
   error_logger:error_info("The ping process failed"),
   {stop, {ping_failed, Error}, State}.
@@ -86,72 +105,74 @@ terminate(_Reason, _State) ->
 code_change(_OldVsn, State, _Extra) ->
   {ok, State}.
 
-%% Pinging
+%% Sessions
+login(User, Pass, API) ->
+  {ok,{{_,200,_},_,ResponseText}} = call("/http/auth", [{user, User}, {password, Pass}, {api_id, API}], true),
+  case parse_response(ResponseText) of
+    {ok, PropList} -> {ok, proplists:get_value(ok, PropList)};
+    {error, Error} -> {stop, Error}
+  end.
+
+ping(SessionID) ->
+  {ok,{{_,200,_},_,ResponseText}} = call("/http/ping", [{session_id, SessionID}], true),
+  case parse_response(ResponseText) of
+    {ok, _}        -> true;
+    {error, Error} -> {error, Error}
+  end.
+
 ping_loop(Time, SessionID) ->
   error_logger:info_msg("Pinging ~s...~n", [SessionID]),
-  case call_ping(SessionID) of
+  case ping(SessionID) of
     true           -> timer:sleep(Time), ping_loop(Time, SessionID);
     {error, Error} -> exit({ping_failed, Error})
   end.
 
 %% Sending
 call_balance(SessionID) ->
-  case call("/http/getbalance", [{session_id, SessionID}]) of
-    {ok, PropList} -> list_to_float(proplists:get_value(credit, PropList));
-    {error, Error} -> {error, Error}
-  end.
+  call("/http/getbalance", [{session_id, SessionID}]).
+handle_balance({ok, PropList}) ->
+  list_to_float(proplists:get_value(credit, PropList));
+handle_balance({error, Error}) ->
+  {error, Error}.
 
 call_check(To, SessionID) ->
-  case call("/utils/routeCoverage.php", [{msisdn, To}, {session_id, SessionID}]) of
-    {ok, _}    -> true;
-    {error, _} -> false
-  end.
+  call("/utils/routeCoverage.php", [{msisdn, To}, {session_id, SessionID}]).
+handle_check({ok, _}) ->
+  true;
+handle_check({error, _}) ->
+  false.
 
 call_cost(MessageID, SessionID) ->
-  case call("/http/getmsgcharge", [{apimsgid, MessageID}, {session_id, SessionID}]) of
-    {ok, PropList} -> str_to_number(proplists:get_value(charge, PropList));
-    {error, Error} -> {error, Error}
-  end.
-
-call_login(User, Pass, API) ->
-  case call("/http/auth", [{user, User}, {password, Pass}, {api_id, API}]) of
-    {ok, PropList} -> proplists:get_value(ok, PropList);
-    {error, Error} -> {error, Error}
-  end.
-
-call_ping(SessionID) ->
-  case call("/http/ping", [{session_id, SessionID}]) of
-    {ok, _}        -> true;
-    {error, Error} -> {error, Error}
-  end.
+  call("/http/getmsgcharge", [{apimsgid, MessageID}, {session_id, SessionID}]).
+handle_cost({ok, PropList}) ->
+  str_to_number(proplists:get_value(charge, PropList));
+handle_cost({error, Error}) ->
+  {error, Error}.
 
 call_send(To, Message, SessionID) ->
-  case call("/http/sendmsg", [{to, To}, {text, Message}, {session_id, SessionID}]) of
-    {ok, PropList} -> proplists:get_value(id, PropList);
-    {error, Error} -> {error, Error}
-  end.
+  call("/http/sendmsg", [{to, To}, {text, Message}, {session_id, SessionID}]).
+handle_send({ok, PropList}) ->
+  proplists:get_value(id, PropList);
+handle_send({error, Error}) ->
+  {error, Error}.
 
 call_status(MessageID, SessionID) ->
-  case call("/http/querymsg", [{apimsgid, MessageID}, {session_id, SessionID}]) of
-    {ok, PropList} -> str_to_number(proplists:get_value(status, PropList));
-    {error, Error} -> {error, Error}
-  end.
+  call("/http/querymsg", [{apimsgid, MessageID}, {session_id, SessionID}]).
+handle_status({ok, PropList}) ->
+  str_to_number(proplists:get_value(status, PropList));
+handle_status({error, Error}) ->
+  {error, Error}.
 
-call(URL, PropList) ->
-  {ok,{{_,200,_},_,ResponseText}} = do_request(URL, PropList),
-  Response = parse_response(ResponseText),
-  case proplists:get_value(err, Response) of
-    undefined -> {ok,    Response};
-    Error     -> {error, Error}
-  end.
+call(Path, PropList) ->
+  call(Path, PropList, false).
 
-do_request(Path, PropList) ->
+call(Path, PropList, Sync) ->
   URL         = ?BASE_URL ++ Path,
   Headers     = [{"User-Agent", "erl-clickatell"}],
   Payload     = proplist_to_params(PropList),
   ContentType = "application/x-www-form-urlencoded",
   HTTPOptions = [],
-  Options     = [],
+  Options     = [{sync, Sync}],
   http:request(post, {URL, Headers, ContentType, Payload}, HTTPOptions, Options).
 
 %% Recieving
@@ -183,7 +204,11 @@ proplist_to_params(PropList) ->
 %% Decoding
 parse_response(Str) ->
   {match, Matches} = regexp:matches(Str, "[A-Za-z]+:"),
-  parse_response_loop(Str, Matches, []).
+  PropList         = parse_response_loop(Str, Matches, []),
+  case proplists:get_value(err, PropList) of
+    undefined -> {ok,    PropList};
+    Error     -> {error, Error}
+  end.
 
 parse_response_loop(Str, Matches, PropList) ->
   case Matches of
